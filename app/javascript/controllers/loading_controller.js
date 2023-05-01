e082477e20036e19da253f39b88bbf48824277ed
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-05-01 10:48:34 +0200

    alright JS for loading

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -1,13 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ 'counter' ]
+  static targets = [ 'counter', 'bar' ]
 
   connect() {
     let count = 0;
+    let height = 0;
     const interval = setInterval(() => {
       count++;
+      height++;
       this.counterTarget.textContent = count;
+      this.barTarget.style.height = `${height}%`;
       if (count == 100) {
         clearInterval(interval);
       }
