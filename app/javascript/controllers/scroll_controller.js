1028b7ffa11018645ec73a661d5a9113bd18361f
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-05-04 22:27:36 +0200

    click from navbar to scroll smoothly to the target div

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -2,6 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="scroll"
 export default class extends Controller {
+  static targets = [ "about", "projects", "contact" ]
+
   connect() {
   }
+
+  scrollToAbout() {
+    this.aboutTarget.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  scrollToProjects() {
+    this.projectsTarget.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  scrollToContact() {
+    this.contactTarget.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
 }
