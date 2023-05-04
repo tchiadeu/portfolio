083fcd19575479d15cd6ef84551a0e0a24eb46bd
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-05-04 22:13:17 +0200

    copy mail to clipboard ok

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,6 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
+  static targets = [ "mail" ]
+
   connect() {
+  }
+
+  copy() {
+    const mail = this.mailTarget.innerText.toLowerCase();
+    const element = document.createElement("textarea");
+    document.body.appendChild(element);
+    element.value = mail;
+    element.select();
+    document.execCommand("copy");
+    document.body.removeChild(element);
   }
 }
