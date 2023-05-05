6ee1e20cf3525610b3800d34676f20911541fa89
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-05-05 16:03:05 +0200

    trying things to JS appear but not working

--- app/javascript/controllers/appear_controller.js
+++ app/javascript/controllers/appear_controller.js
@@ -2,6 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="appear"
 export default class extends Controller {
+  static targets = [ "leftAppear", "bottomAppear" ]
+
   connect() {
+    console.log(this.leftAppearTargets)
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.addAnimation()
+        }
+      })
+    })
+    Array.from(this.targets).forEach((target) => {
+      this.observer.observe(target)
+    })
+  }
+
+  addAnimation() {
+    this.leftAppearTargets.forEach((target) => {
+      target.classList.add("slide-in-left")
+    })
+    this.bottomAppearTargets.forEach((target) => {
+      target.classList.add("slide-in-bottom")
+    })
+  }
+
+  disconnect() {
+    this.observer.disconnect()
   }
 }
