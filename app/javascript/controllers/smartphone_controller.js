1827e19885c2d9caa1c671f560f43aecf049e873
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-05-08 17:25:15 +0200

    fixing notion font-size first page

--- app/javascript/controllers/smartphone_controller.js
+++ app/javascript/controllers/smartphone_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="smartphone"
 export default class extends Controller {
+  static targets = [ "navbarName", "navbarBurger", "navbarOptions", "about", "projects", "contact" ]
   connect() {
+  }
+
+  navbarOptions() {
+    this.aboutTarget.style.display = "none";
+    this.projectsTarget.style.display = "none";
+    this.contactTarget.style.display = "none";
   }
 }
