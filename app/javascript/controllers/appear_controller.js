6a92e3f1fc1aa04fd191b6288ab8df055850bd03
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-05-31 18:53:01 +0200

    better design for appear logic

--- app/javascript/controllers/appear_controller.js
+++ app/javascript/controllers/appear_controller.js
@@ -2,159 +2,149 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="appear"
 export default class extends Controller {
-  static targets = [ "blackAppear", "aboutAppear", "contactAppear", "projectAppear", "project", "category" ]
+  static targets = [ "aboutAppear", "contactAppear", "projectAppear", "content" ]
 
   connect() {
     const body = document.body
-    let firstCategoryAppear = true
+    let firstTime = true
 
     const ratio = 0.3
     const projectRatio = 0.1
-    const bodyRatio = 0.2
 
     const options = {
-      root: document.querySelector("#scrollArea"),
-      rootMargin: "100px 0px",
-      threshold: ratio
-    }
-    const projectOptions = {
-      root: document.querySelector("#scrollArea"),
-      rootMargin: "185px 0px",
-      threshold: projectRatio
-    }
-    const bodyOptions = {
       root: null,
       rootMargin: "0px",
-      threshold: bodyRatio
+      threshold: ratio
     }
 
-    const blackHandleIntersect = (entries) => {
+    const contentHandleIntersect = (entries) => {
       entries.forEach((entry) => {
-        if (body.classList.contains("change-to-white") && entry.intersectionRatio > ratio) {
+        const targetIndex = this.contentTargets.findIndex((target) => target == entry.target)
+        if (entry.intersectionRatio > ratio
+            && firstTime === false
+            && (targetIndex === this.contentTargets.length - 1
+            || targetIndex === this.contentTargets.length - 2)) {
+          body.classList.remove("change-to-black")
+          body.classList.add("change-to-white")
+          setTimeout(() => {
+            entry.target.classList.remove("unreveal")
+            if (entry.target.classList.contains("arrow-down")) {
+              entry.target.style.animation = "rebound 1000ms cubic-bezier(0.4, 0, 1, 1) infinite"
+            } else {
+              entry.target.style.animation = "image-appear 1000ms ease-in-out forwards"
+            }
+          }, 200)
+        } else if (body.classList.contains("change-to-white")
+                  && entry.intersectionRatio > ratio) {
           entry.target.classList.remove("unreveal")
           entry.target.classList.add("reveal")
-        } else {
-          entry.target.classList.remove("reveal")
+        } else if (body.classList.contains("change-to-black")) {
+          firstTime = false
+          entry.target.classList.add("unreveal")
+          setTimeout(() => {
+            entry.target.classList.remove("image-reveal")
+            entry.target.classList.remove("slide-in-arrow")
+            entry.target.classList.remove("slide-in-navbar")
+            entry.target.classList.remove("slide-in-title")
+            entry.target.classList.remove("slide-in-description")
+            entry.target.classList.remove("slide-in-button")
+            entry.target.classList.remove("reveal")
+          }, 500)
         }
       })
     }
-    const whiteHandleIntersect = (entries) => {
+
+    const aboutHandleIntersect = (entries) => {
       entries.forEach((entry) => {
-        if (body.classList.contains("change-to-black") && entry.intersectionRatio > ratio) {
+        const targetIndex = this.aboutAppearTargets.findIndex((target) => target == entry.target)
+        if ((targetIndex === 0
+            || targetIndex === this.aboutAppearTargets.length - 1)
+            && entry.intersectionRatio > ratio) {
+          body.classList.remove("change-to-white")
+          body.classList.add("change-to-black")
+          setTimeout(() => {
+            entry.target.classList.remove("unreveal")
+            entry.target.classList.add("reveal")
+          }, 200)
+        } else if (body.classList.contains("change-to-black") && entry.intersectionRatio > ratio) {
           entry.target.classList.remove("unreveal")
           entry.target.classList.add("reveal")
-        } else {
-          entry.target.classList.remove("reveal")
+        } else if (body.classList.contains("change-to-white")) {
+          entry.target.classList.add("unreveal")
+          setTimeout(() => {
+            entry.target.classList.remove("reveal")
+          }, 500)
         }
       })
     }
+
     const projectHandleIntersect = (entries) => {
       entries.forEach((entry) => {
-        if (body.classList.contains("change-to-white") && entry.intersectionRatio > projectRatio ) {
+        const targetIndex = this.projectAppearTargets.findIndex((target) => target == entry.target)
+        if ((targetIndex === 0
+            || targetIndex === this.projectAppearTargets.length - 1
+            || targetIndex === this.projectAppearTargets.length - 2)
+            && entry.intersectionRatio > ratio) {
+          body.classList.remove("change-to-black")
+          body.classList.add("change-to-white")
+          setTimeout(() => {
+            entry.target.classList.remove("unreveal")
+            entry.target.classList.add("reveal")
+          }, 200)
+        } else if (body.classList.contains("change-to-white") && entry.intersectionRatio > projectRatio) {
           entry.target.classList.remove("unreveal")
           entry.target.classList.add("reveal")
-        } else {
-          entry.target.classList.remove("reveal")
+        } else if (body.classList.contains("change-to-black")) {
+          entry.target.classList.add("unreveal")
+          setTimeout(() => {
+            entry.target.classList.remove("reveal")
+          }, 500)
         }
       })
     }
-    const bodyHandleIntersect = (entries) => {
+
+    const contactHandleIntersect = (entries) => {
       entries.forEach((entry) => {
-        const targetIndex = this.categoryTargets.findIndex((target) => target == entry.target)
-        if (entry.intersectionRatio > bodyRatio && targetIndex !== -1) {
-          if (firstCategoryAppear) {
-            firstCategoryAppear = false
-          } else {
-            if (targetIndex === 0) {
-              body.classList.remove("change-to-black")
-              body.classList.add("change-to-white")
-              this.aboutAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.contactAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.projectAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.projectTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-            } else if (targetIndex === 1) {
-              body.classList.add("change-to-black")
-              body.classList.remove("change-to-white")
-              this.projectAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.projectTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.contactAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              setTimeout(() => {
-                this.aboutAppearTargets[0].classList.remove("unreveal")
-                this.aboutAppearTargets[1].classList.remove("unreveal")
-                this.aboutAppearTargets[0].classList.add("reveal")
-                this.aboutAppearTargets[1].classList.add("reveal")
-              }, 200);
-            } else if (targetIndex === 2) {
-              body.classList.remove("change-to-black")
-              body.classList.add("change-to-white")
-              this.aboutAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.contactAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.projectAppearTargets[0].classList.remove("unreveal")
-              this.projectAppearTargets[0].classList.add("reveal")
-            } else if (targetIndex === 3) {
-              body.classList.add("change-to-black")
-              body.classList.remove("change-to-white")
-              this.aboutAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.projectAppearTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              this.projectTargets.forEach(target => {
-                target.classList.add("unreveal")
-              })
-              setTimeout(() => {
-                this.contactAppearTargets[0].classList.remove("unreveal")
-                this.contactAppearTargets[0].classList.add("reveal")
-              }, 200);
-            }
-          }
+        const targetIndex = this.contactAppearTargets.findIndex((target) => target == entry.target)
+        if ((targetIndex === 0
+            || targetIndex === this.contactAppearTargets.length - 1)
+            && entry.intersectionRatio > ratio) {
+          body.classList.remove("change-to-white")
+          body.classList.add("change-to-black")
+          setTimeout(() => {
+            entry.target.classList.remove("unreveal")
+            entry.target.classList.add("reveal")
+          }, 200)
+        } else if (body.classList.contains("change-to-black") && entry.intersectionRatio > ratio) {
+          entry.target.classList.remove("unreveal")
+          entry.target.classList.add("reveal")
+        } else if (body.classList.contains("change-to-white")) {
+          entry.target.classList.add("unreveal")
+          setTimeout(() => {
+            entry.target.classList.remove("reveal")
+          }, 500)
         }
       })
     }
 
-    const blackObserver = new IntersectionObserver(blackHandleIntersect, options)
+    const contentObserver = new IntersectionObserver(contentHandleIntersect, options)
+    this.contentTargets.forEach(target => {
+      contentObserver.observe(target)
+    })
+
+    const aboutObserver = new IntersectionObserver(aboutHandleIntersect, options)
+    this.aboutAppearTargets.forEach(target => {
+      aboutObserver.observe(target)
+    })
+
+    const projectObserver = new IntersectionObserver(projectHandleIntersect, options)
     this.projectAppearTargets.forEach(target => {
-      blackObserver.observe(target)
-    })
-
-    const whiteObserver = new IntersectionObserver(whiteHandleIntersect, options)
-    this.aboutAppearTargets.forEach((target, index) => {
-      if (index !== 1) {
-        whiteObserver.observe(target)
-      }
-    })
-    this.contactAppearTargets.forEach(target => {
-      whiteObserver.observe(target)
-    })
-
-    const projectObserver = new IntersectionObserver(projectHandleIntersect, projectOptions)
-    this.projectTargets.forEach(target => {
       projectObserver.observe(target)
     })
-    projectObserver.observe(this.aboutAppearTargets[1])
 
-    const bodyObserver = new IntersectionObserver(bodyHandleIntersect, bodyOptions)
-    this.categoryTargets.forEach(target => {
-      bodyObserver.observe(target)
+    const contactObserver = new IntersectionObserver(contactHandleIntersect, options)
+    this.contactAppearTargets.forEach(target => {
+      contactObserver.observe(target)
     })
   }
 }
