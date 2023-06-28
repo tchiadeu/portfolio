493a62d353269e835d21f28d4768c13e57c6c7c1
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-06-28 10:45:25 +0200

    add routes to controller pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
+  get 'about', to: 'pages#about'
 end
