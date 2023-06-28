5fe98cf68fb9b8a6522bed7fd659af80deb44258
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-06-28 11:28:18 +0200

    add CRUD methods to clients controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,39 @@
 class ClientsController < ApplicationController
+  def index
+    @clients = Client.all
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    @client.save
+    redirect_to @client
+  end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    @client.update(client_params)
+  end
+
+  def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :phone_number, :email, :address, :post_code, :city, :siret_number, :tva_number)
+  end
 end
