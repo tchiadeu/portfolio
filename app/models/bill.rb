5fa00f9f70628ffa7336a828d57eed257b655442
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-06-28 11:30:25 +0200

    add validations to models client & model bill

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,7 @@
 class Bill < ApplicationRecord
+  belongs_to :client
+
+  validates :client, presence: true
+  validates :unity, presence: true
+  validates :quantity, presence: true
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,5 @@
 class Client < ApplicationRecord
+  has_many :bills
+
+  validates :name, presence: true
 end
