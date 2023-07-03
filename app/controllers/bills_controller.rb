7bdf21c51574ca5cc6b487e8871bc54429ee4657
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-07-03 16:59:13 +0200

    refacto bills controller"

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -21,51 +21,9 @@ class BillsController < ApplicationController
   end
 
   def create
-    @bill = Bill.new(bill_params)
-    @bill.admin = current_admin
-    @bill.month = Date.today.month
-    @bill.year = Date.today.year
-    @bill.number = (Bill.where(year: @bill.year).count + 1)
-    @bill.emission_date = Date.today
-    @bill.due_date = Date.today + 30
-
-    items = []
-    10.times do |i|
-      item_params = params[:bill][:item][i.to_s]
-      item = Item.new(
-        name: item_params[:name],
-        description: item_params[:description],
-        unity: item_params[:unity],
-        quantity: item_params[:quantity],
-        unit_price: item_params[:unit_price]
-      )
-      item.total_price = item.quantity.to_f * item.unit_price.to_f
-      items << item if item.name.present?
-    end
-    @bill.items = items
-
-    # item_params = params[:bill][:items_attributes]
-    # if item_params.present?
-    #   item_params.each do |item_param|
-    #     item_param[:total_price] = item_param[:quantity].to_f * item_param[:unit_price].to_f
-    #     @bill.items.build!(item_param)
-    #   end
-    # end
-
-    client = Client.new(
-      name: params[:bill][:client_attributes][:name],
-      address: params[:bill][:client_attributes][:address],
-      post_code: params[:bill][:client_attributes][:post_code],
-      city: params[:bill][:client_attributes][:city],
-      siret_number: params[:bill][:client_attributes][:siret_number],
-      tva_number: params[:bill][:client_attributes][:tva_number],
-      email: params[:bill][:client_attributes][:email],
-      phone_number: params[:bill][:client_attributes][:phone_number]
-    )
-    client.admin = current_admin
-    client.save
-    @bill.client = client
-
+    build_bill
+    build_client
+    build_items
     if @bill.save
       redirect_to dashboard_path
     else
@@ -88,6 +46,47 @@ class BillsController < ApplicationController
   # end
 
   private
+
+  def build_client
+    client = Client.new(
+      name: params[:bill][:client_attributes][:name],
+      address: params[:bill][:client_attributes][:address],
+      post_code: params[:bill][:client_attributes][:post_code],
+      city: params[:bill][:client_attributes][:city],
+      siret_number: params[:bill][:client_attributes][:siret_number],
+      tva_number: params[:bill][:client_attributes][:tva_number],
+      email: params[:bill][:client_attributes][:email],
+      phone_number: params[:bill][:client_attributes][:phone_number]
+    )
+    client.admin = current_admin
+    client.save
+    @bill.client = client
+  end
+
+  def build_bill
+    @bill = Bill.new(bill_params)
+    @bill.admin = current_admin
+    @bill.month = Date.today.month
+    @bill.year = Date.today.year
+    @bill.number = (Bill.where(year: @bill.year).count + 1)
+    @bill.emission_date = Date.today
+    @bill.due_date = Date.today + 30
+  end
+
+  def build_items
+    10.times do |i|
+      item_params = params[:bill][:item][i.to_s]
+      item = Item.new(
+        name: item_params[:name],
+        description: item_params[:description],
+        unity: item_params[:unity],
+        quantity: item_params[:quantity],
+        unit_price: item_params[:unit_price]
+      )
+      item.total_price = item.quantity.to_f * item.unit_price.to_f
+      @bill.items << item if item.name.present?
+    end
+  end
 
   def bill_params
     params.require(:bill).permit(
