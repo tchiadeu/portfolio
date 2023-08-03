cb09e11fe72fee2de7703666f409b4893c0196c8
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-08-03 21:24:03 +0200

    add visibility hidden in appear logic

--- app/javascript/controllers/appear_controller.js
+++ app/javascript/controllers/appear_controller.js
@@ -29,14 +29,16 @@ export default class extends Controller {
         if (targetIndex === 0 && entry.intersectionRatio > ratio) {
           body.classList.remove("change-to-white")
           body.classList.add("change-to-black")
+          this.contentContainerTarget.style.visibility = "hidden"
           setTimeout(() => {
             entry.target.classList.remove("unreveal")
             entry.target.classList.add("reveal")
             aboutObserver.unobserve(entry.target)
           }, 200)
           setTimeout(() => {
+          this.contentContainerTarget.style.visibility = "visible"
           this.contentContainerTarget.classList.add("change-to-white")
-          }, 3000)
+        }, 3000)
         } else if (entry.intersectionRatio > ratio) {
           entry.target.classList.remove("unreveal")
           entry.target.classList.add("reveal")
@@ -56,12 +58,14 @@ export default class extends Controller {
         if (targetIndex === 0 && entry.intersectionRatio > projectRatio) {
           body.classList.remove("change-to-black")
           body.classList.add("change-to-white")
+          this.aboutContainerTarget.style.visibility = "hidden"
           setTimeout(() => {
             entry.target.classList.remove("unreveal")
             entry.target.classList.add("reveal")
             projectObserver.unobserve(entry.target)
           }, 200)
           setTimeout(() => {
+            this.contentContainerTarget.style.visibility = "visible"
             this.aboutContainerTarget.classList.add("change-to-black")
           }, 3000)
         } else if (entry.intersectionRatio > projectRatio) {
@@ -83,12 +87,14 @@ export default class extends Controller {
         if (targetIndex === 0 && entry.intersectionRatio > ratio) {
           body.classList.remove("change-to-white")
           body.classList.add("change-to-black")
+          this.projectContainerTarget.style.visibility = "hidden"
           setTimeout(() => {
             entry.target.classList.remove("unreveal")
             entry.target.classList.add("reveal")
             contactObserver.unobserve(entry.target)
           }, 200);
           setTimeout(() => {
+            this.contentContainerTarget.style.visibility = "visible"
             this.projectContainerTarget.classList.add("change-to-white")
           }, 3000)
         } else if (entry.intersectionRatio > ratio) {
